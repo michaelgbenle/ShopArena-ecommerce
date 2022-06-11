34ffa8af9db8c97210d10b1fa94f074019571b2d
Michael <michaelgbenle@gmail.com>
2022-06-11 19:01:20 +0100

    email validation

--- database/postgres.go
+++ database/postgres.go
@@ -336,6 +336,15 @@ func (pdb *PostgresDb) CreateBuyer(user *models.Buyer) (*models.Buyer, error) {
 	return user, err
 }
 
+// ValidateBuyer finds updates buyer status to true
+func (pdb *PostgresDb) ValidateBuyer(email string) error {
+	buyer := &models.Buyer{}
+	if err := pdb.DB.Model(buyer).Where("email =?", email).Update("is_active", true).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
 //CreateBuyerCart creates a new cart for the buyer
 func (pdb *PostgresDb) CreateBuyerCart(cart *models.Cart) (*models.Cart, error) {
 	var err error
